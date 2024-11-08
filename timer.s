0032ce6444c50277d9ba485619a95c8d9603efb5
marinbarbos <marinabarbosa@protonmail.com>
2024-11-08 10:25:44 -0300

    Timer first draft

--- timer.s
+++ timer.s
@@ -7,34 +7,30 @@ Marina Barbosa Américo
 
 /**************************************************************************/
 /* Main Program                                                           */
-/*   Escreva um programa que implementa uma tarefa do estilo “maquina     */
-/*   de escrever”; ou seja, leia cada caracter que e recebido pela JTAG   */
-/*   UART a partir do computador hospedeiro e entao mostre o caracter     */
-/*   na janela do terminal do programa monitor. Use a tecnica de polling  */
-/*   para determinar se um novo caracter esta disponıvel na JTAG UART.    */
+/*   Determines the maximum number of consecutive 1s in a data word.      */
 /*                                                                        */
-/* r8   - Data from data register DATA field      DATA mask: 0xFF         */
-/* r9   - RVALID data from data register          RVALID mask: 0x8000     */
-/* r10  - RAVAIL from data & WSPACE from control  RAVAIL mask: 0xFFFF0000 */
-/* r11  - AC data from controller register        AC mask: 0x400          */
-/* r12  - no idea tbh                                                     */
-/* r13  - aaaaaaaaaaaaaaaa                                                */
-/* r14  - Timer address                                                   */
-/* r15  - UART Data register address                                      */
+/* r8   - Contains the switch data                                        */
+/* r9   - Switch data sum                                                 */
+/* r10  - Slide switch address for use                                    */
+/* r11  - Green LED address for use                                       */
+/* r12  - Push Button address for use                                     */
+/* r13  - Contains Push Button data                                       */
 /**************************************************************************/
 
 /*Tratamento da interrupção */ 
 # 0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F
-.equ RED_ADDRESS, 0x10000000
-.equ TIMER_ADDRESS, 0x10002000
-.equ SWITCHES_ADDRESS, 0x10000040
-.org 0x20
 
+ARR_DISPLAY:
+    # .byte 0x7E, 0x30, 0x6D, 0x79, 0x33, 0x5B, 0x5F, 0x70, 0x7F, 0x7B
+    .byte 0x7E, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7F, 0x67
+    
+    
+    .org 0x20
 RTI:
     rdctl et, ipending
     beq et, r0, OTHER_EXCEPTIONS
-
     subi ea, ea, 4    
+
     andi r12, et, 1
     beq r12, r0, OTHER_EXCEPTIONS
     call TIMER_EXEC
@@ -42,40 +38,78 @@ RTI:
 OTHER_EXCEPTIONS:
     eret
 
+/*Sub Rotina - de display */
+    .org 0x100    
 TIMER_EXEC:
-  movia r14, TIMER_ADDRESS
+  movia r14, 0x10002000
   stwio r0, (r14)
-  movi r12, 1               # Inicializa r12 com 1
-  movia r14, SWITCHES_ADDRESS /* SW slider switch base address */
-  ldwio r8, 0(r14)
-  beq r8, r12, DESLOCA_DIREITA
-  sll r12, r12, r9         # Desloca o bit 1 para a posição em r11
-  stwio r12, 0(r15)
-  addi r9, r9, 1
-ACENDE_LEDS:
-ret
+  stwio  r13, (r15)
 
-DESLOCA_DIREITA:
-  subi r9, r9, 1
-  sll r12, r12, r9         # Desloca o bit 1 para a posição em r11
-  stwio r12, 0(r15)
-  br ACENDE_LEDS
+  addi r7, r7, 1 /* Precisa de um registrador ou local no stack frame fixo pra guardar*/
+  addi r11, r11, 0x270F
+  blt r7, r11, CONVERT_DISPLAY:
+  mov r7, r0
+
+CONVERT_DISPLAY:
+  movi r6, 0
+  LOOP_DEC:
+    subi r7, r7, 10
+    addi r6, r6, 1
+    blt r7, 
+
+  # SEGUNDO DISPLAY(0010)
+  movia r13, ARR_DISPLAY
+  srli r12, r7, 4
+  andi r12, r12, 0x0F
+  add r13, r13, r12
+  ldb r13, (r13)
+  stbio r13, 1(r10)
+  
+  # PRIMEIRO DISPLAY(0001)
+  movia r13, ARR_DISPLAY
+  andi r12, r7, 0x0F
+  add r13, r13, r12
+  ldb r13, (r13)
+  stbio r13, (r10)
+
+  /*
+  # TERCEIRO DISPLAY(0100)
+  movia r13, ARR_DISPLAY
+  srli r12, r7, 8
+  andi r12, r12, 0x0F
+  add r13, r13, r12
+  ldb r13, (r13)
+  stbio r13, 2(r10)
+
+  # QUARTO DISPLAY(1000)
+  movia r13, ARR_DISPLAY
+  srli r12, r7, 12
+  andi r12, r12, 0x0F
+  add r13, r13, r12
+  ldb r13, (r13)
+  stbio r13, 3(r10)    */
+
+  stwio r0, 12(r15)
+
+ret
 
 /*Configura as variaveis */
 .global _start
 _start:
 
-movia r15, RED_ADDRESS /* Load controller address */
-movi r9, 0
+mov r7, r0/*Variable that acumulates the total */
+movia r10, 0x10000020 /* Load 7 segments display address */
+
+movia r15, 0x10001000 /* Load controller address */
 
 /* Start timer interrupt config */
-movia r14, TIMER_ADDRESS
+movia r14, 0x10002000
 
-movia r8, 25000000 /* half second */
+movia r8, 10000000 /* 200ms */
 stwio r8, 8(r14) /* lower counter part */
 
-srli r10, r8, 16 /* higher counter part */
-stwio r10, 12(r14) 
+srli r9, r8, 16 /* higher counter part */
+stwio r9, 12(r14) 
 
 movi r11, 0b111
 stwio r11, 4(r14) /* init timer */
@@ -87,7 +121,8 @@ wrctl ienable, r14 /*Enable timer */
 movia r8, 1
 wrctl status, r8 
 
-LOOP:
-  br LOOP
+LACO:
+  br LACO
 
 .end
+
