cccc9d0d263f62591bdfc077aea9bdbd9baf04ea
Marina Barbosa <mb.americo@unesp.br>
2024-11-21 20:10:43 -0300

    constants em portugues

--- new-menu/constants.s
+++ new-menu/constants.s
@@ -1,9 +1,8 @@
-.equ RED_LIMIT, 0x11          /* Posição máxima dos LEDs vermelhos (17) */
-.equ RED_ADDRESS, 0x10000000 /* Endereço base dos LEDs vermelhos */
-.equ BREAK_LINE, 0x0a        /* Código ASCII para nova linha */
-.equ TIMER_ADDRESS, 0x10002000
-.equ SWITCHES_ADDRESS, 0x10000040
-.equ SEV_SEG_MASK, 0x0F
-.equ SEV_SEG_ADDR, 0x10000020 /* Load 7 segments display address */
-.equ LOAD_CONTR_ADDR, 0x10001000 /* Load controller address */
-.equ FIRST_PUSH_BUTTON_ADDRESS, 0x1000005C
+.equ RED_LIMIT, 0x11                        /* Contagem maxima dos LEDs (17) */
+.equ RED_ADDRESS, 0x10000000                /* Endereco dos LEDs vermelhos */
+.equ BREAK_LINE, 0x0a                       /* Codigo ASCII para New line(\n) */
+.equ TIMER_ADDRESS, 0x10002000              /* Endereco do Clock */
+.equ SWITCHES_ADDRESS, 0x10000040           /* Endereco dos Toggle Switches */
+.equ SEV_SEG_MASK, 0x0F                     /* Mascara para o uso do display de 7 segmentos */
+.equ SEV_SEG_ADDR, 0x10000020               /* Endereco do display de 7 segmentos */
+.equ LOAD_CONTR_ADDR, 0x10001000            /* Endereco do Load Controller */
